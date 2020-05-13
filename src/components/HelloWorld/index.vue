ec19c2d6c5e32003bc985bae240a13c8eac30444
Didier Margarido <didier.margarido@namecheap.com>
2020-05-13 17:49:37 +0100

    Updated HelloWorld

--- src/components/HelloWorld/index.vue
+++ src/components/HelloWorld/index.vue
@@ -47,54 +47,6 @@
         </a>
       </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-          rel="noopener"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-          rel="noopener"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-          rel="noopener"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-          rel="noopener"
-        >
-          Twitter
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://news.vuejs.org"
-          target="_blank"
-          rel="noopener"
-        >
-          News
-        </a>
-      </li>
-    </ul>
     <h3>Ecosystem</h3>
     <ul>
       <li>
@@ -148,7 +100,7 @@
 
 <script>
 export default {
-  name: "HelloWorld",
+  name: 'HelloWorld',
   props: {
     msg: String
   }
